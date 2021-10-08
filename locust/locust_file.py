8c791e48193010e8e502c772dd8f126c1f45302d
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-10-08 14:00:32 +0200

    Update locust_file.py

--- locust/locust_file.py
+++ locust/locust_file.py
@@ -1,12 +1,13 @@
 from locust import HttpUser, task, between
 from random import randint
 
+
+host = "http://127.0.0.1:5000"
+
+
 COMPETITION = "Spring Festival",
 
 CLUB = "Simply Lift"
-# amélio randint
-
-
 
 
 class User(HttpUser):
@@ -15,35 +16,34 @@ class User(HttpUser):
     @task(10)
     def index(self):
         """Declared a task with the index method with a higher weight (10)."""
-        self.client.get("/")
+        self.client.get(f"{host}/")
 
     @task(3)
     def logout(self):
         """Declared a task with the logout method with a higher weight (3)."""
-        self.client.get("/logout")
+        self.client.get(f"{host}/logout")
 
     @task(1)
     def show_clubs(self):
         """Declared a task with the show_clubs method with a higher weight (1)."""
-        self.client.get("/board")
+        self.client.get(f"{host}/board")
 
     @task(8)
     def login(self):
         """Declared a task with the show_summary method with a higher weight (8)."""
         email_address = "john@simplylift.co"
         #faire un adresse en erreur
-        self.client.post("/showSummary", {"email": email_address})
+        self.client.post(f"{host}/showSummary", {"email": email_address})
 
     @task(3)
     def purchase_places(self):
         """Declared a task with the purchase_places method with a higher weight (3)."""
         places = randint(1, 50)
-        self.client.post(
-            "/purchasePlaces",
-            {"competition": competition, "club": CLUB, "places": COMPETITION},
+        self.client.post(f"{host}/purchasePlaces",
+            {"competition": places, "club": CLUB, "places": COMPETITION},
         )
 
     @task(6)
     def book(self):
         """Declared a task with the book method with a higher weight (6)."""
-        self.client.get(f"/book/{COMPETITION}/{CLUB}")
+        self.client.get(f"{host}/book/{COMPETITION}/{CLUB}")
